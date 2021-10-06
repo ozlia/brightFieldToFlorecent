0ef4891aeb37cf73c09c0841744fab6907821896
ozlia <63154309+ozlia@users.noreply.github.com>
2021-10-07 00:09:56 +0300

    create basic ae

---
+++ BasicAE/data_prepere.py
@@ -0,0 +1,28 @@
+import os
+from aicsimageio import AICSImage
+import numpy as np
+
+
+def load(org_type):
+    fovs = []
+    folder = os.listdir("/storage/users/assafzar/fovs/" + org_type)
+    for t in folder:
+        if ".tiff" in folder:
+            fovs.append("/storage/users/assafzar/fovs/" + org_type + t)
+
+    return fovs
+
+
+def separate_data(fovs):
+    bright_field = []
+    fluorescent = []
+    for tiff in fovs:
+        reader = AICSImage(tiff)
+        img = reader.data
+
+        img = np.squeeze(img, axis=0)
+        n_channels = img.shape[0]
+        mid_slice = np.int(0.5 * img.shape[1])
+        bright_field.append(img[n_channels-1, mid_slice, :, :])
+        fluorescent.append(img[n_channels-2, mid_slice, :, :])
+    return bright_field, fluorescent
---
+++ BasicAE/run.py
@@ -0,0 +1,5 @@
+from BasicAE import data_prepere
+
+org_type = "Mitochondria/"
+data_input, data_output = data_prepere.separate_data(data_prepere.load(org_type))
+q=0
