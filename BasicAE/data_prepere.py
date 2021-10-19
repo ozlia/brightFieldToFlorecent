098529f8413a04d93172187c91e46f5d63c6f14d
ozlia <ozlia@post.bgu.ac.il>
2021-10-19 17:21:37 +0300

    fixed dim problems

--- BasicAE/data_prepere.py
+++ BasicAE/data_prepere.py
@@ -8,14 +8,14 @@ import cv2
 def load(org_type):
     fovs = []
     folder = os.listdir("/storage/users/assafzar/fovs/" + org_type)
+    counter = 0
     for t in folder:
-        if ".tiff" in folder:
+        if ".tiff" in t:
             fovs.append("/storage/users/assafzar/fovs/" + org_type + t)
-
     return fovs
 
-
-def separate_data(fovs):
+                ## files, x_pixels, y_pixels, color
+def separate_data(fovs, x, y, z):
     bright_field = []
     fluorescent = []
     for tiff in fovs:
@@ -25,7 +25,15 @@ def separate_data(fovs):
         img = np.squeeze(img, axis=0)
         n_channels = img.shape[0]
         mid_slice = np.int(0.5 * img.shape[1])
+        img_resized = img[n_channels-1, mid_slice, :, :]
+        img_resized = cv2.cvtColor(img_resized, cv2.COLOR_BGR2RGB)
+        img_resized = cv2.resize(img_resized, (x, y))
+        bright_field.append(img_resized)
+        img_resized = img[n_channels-2, mid_slice, :, :]
+        img_resized = cv2.cvtColor(img_resized, cv2.COLOR_BGR2RGB)
+        img_resized = cv2.resize(img_resized, (x, y))
+        fluorescent.append(img_resized)
 
-        bright_field.append(cv2.resize(img[n_channels-1, mid_slice, :, :], (128, 128)))
-        fluorescent.append(cv2.resize(img[n_channels-2, mid_slice, :, :], (128, 128)))
-    return bright_field, fluorescent
+    bright_field_array = np.reshape(bright_field, (len(bright_field), x, y, z))
+    fluorescent_array = np.reshape(fluorescent, (len(fluorescent), x, y, z))
+    return bright_field_array, fluorescent_array
--- BasicAE/run.py
+++ BasicAE/run.py
@@ -3,8 +3,6 @@ import aearc
 import keras
 img_size = (128, 128)
 num_classes = 3
-train_number = 80
-
 
 org_type = "Mitochondria/"
 data_input, data_output = data_prepere.separate_data(data_prepere.load(org_type))
@@ -16,12 +14,14 @@ keras.backend.clear_session()
 model = aearc.get_model(img_size)
 model.summary()
 
-train_data_input = data_input[:80]
-train_data_output = data_output[:80]
-test_data_input = data_input[80:]
-test_data_output = data_output[80:]
+train_number = int(len(data_input)*(3/4))
+
+train_data_input = data_input[:train_number]
+train_data_output = data_output[:train_number]
+test_data_input = data_input[train_number:]
+test_data_output = data_output[train_number:]
 callbacks = [
     keras.callbacks.ModelCheckpoint("BasicAEModel.h5", save_best_only=True)
 ]
-model.fit(train_data_input, train_data_output, epochs=15, shuffle=True, callbacks=callbacks)
+model.fit(train_data_input, train_data_output, epochs=1, shuffle=True, callbacks=callbacks)
 model.save("basicAE/model/")
