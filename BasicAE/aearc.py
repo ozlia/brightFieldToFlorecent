814c2d324fbaab82a404239b71fe7c4f2a766526
ozlia <ozlia@post.bgu.ac.il>
2021-10-21 10:21:56 +0300

    diffrent loss func

--- BasicAE/aearc.py
+++ BasicAE/aearc.py
@@ -27,7 +27,7 @@ def get_model(img_size):
     outputs = layers.Conv2D(1, 3, activation="sigmoid", padding="same")(x)
 
     model = keras.Model(inputs, outputs)
-    model.compile(optimizer="adam", loss="mean_squared_error", metrics = ["accuracy"])
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics = ["accuracy"])
     return model
 
 
