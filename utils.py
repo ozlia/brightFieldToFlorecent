7b57693ae56db8faa1a0127457c1f8a417bd826f
omeriku <73793617+omeriku@users.noreply.github.com>
2021-12-20 21:37:54 +0200

    updated utils.py to be generic

--- utils.py
+++ utils.py
@@ -1,21 +1,29 @@
 import PIL
 import numpy as np
 from PIL import Image
+import os
+import getpass
 
+DIRECTORY = "prediction3D"
+USER = getpass.getuser()
 
 def save_entire_patch_series(input_patches, output_patches):
+    global DIRECTORY
     for i in range(0, 27):
         Image.fromarray(np.squeeze(input_patches[i]) * 255).convert('L').save(
-            '/home/ozlia/prediction2D/input_patch' + str(i) + '.png')
+            '/home/%s/%s/input_patch_%d.png' % (USER, DIRECTORY, i))
         Image.fromarray(np.squeeze(output_patches[i]) * 255).convert('L').save(
-            '/home/ozlia/prediction2D/output_patch' + str(i) + '.png')
+            '/home/%s/%s/output_patch_%d.png' % (USER, DIRECTORY, i))
 
 
 def save_img(data_input, data_output, predictions):
     print("saving first image")
-    Image.fromarray(np.squeeze(data_input) * 255).convert('L').save('/home/ozlia/prediction2D/input.png')
-    Image.fromarray(np.squeeze(predictions) * 255).convert('L').save('/home/ozlia/prediction2D/prediction.png')
-    Image.fromarray(np.squeeze(data_output) * 255).convert('L').save('/home/ozlia/prediction2D/orginal.png')
+    global DIRECTORY
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    Image.fromarray(np.squeeze(data_input) * 255).convert('L').save('/home/%s/%s/input.png' % (USER, DIRECTORY))
+    Image.fromarray(np.squeeze(predictions) * 255).convert('L').save('/home/%s/%s/prediction.png' % (USER, DIRECTORY))
+    Image.fromarray(np.squeeze(data_output) * 255).convert('L').save('/home/%s/%s/original.png' % (USER, DIRECTORY))
 
     # todo needs work on defining metrics
     def evaluate(test_data_input, test_data_output, metrics):
