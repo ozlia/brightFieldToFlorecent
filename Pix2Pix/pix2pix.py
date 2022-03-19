0a41a6f2b3f8116754f6a12870e3b09119b8e5d0
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2022-03-19 11:06:02 +0200

    changes to pix2pix

--- Pix2Pix/pix2pix.py
+++ Pix2Pix/pix2pix.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 
 import sys
-
 from tensorflow.keras.utils import plot_model
 from patchify import unpatchify
 
@@ -9,8 +8,10 @@ import utils
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Model, load_model, save_model
-from tensorflow.keras.layers import Input, Dropout, Concatenate, BatchNormalization, LeakyReLU, UpSampling2D, Conv2D,Activation
+from tensorflow.keras.layers import Input, Dropout, Concatenate, BatchNormalization, LeakyReLU, UpSampling2D, Conv2D, \
+    Activation,Flatten,Dense
 from tensorflow.keras.optimizers import Adam
+# from Pix2Pix.custom_metrics import wasserstein_loss
 
 import datetime
 import matplotlib.pyplot as plt
@@ -35,22 +36,19 @@ class Pix2Pix:
         self.channels = self.img_shape[2]
 
         # Calculate patch size of D (PatchGAN)
-        patchGAN_patch_size = 2 ** 4
-        # patchGAN_patch_size = 2 ** 5 #TODO needs fixing for arc
+        patchGAN_patch_size = 2 ** 4  # 2 ** 5
         patch = int(self.img_shape[1] / patchGAN_patch_size)
-        self.disc_patch = (patch, patch, self.img_shape[0])
+        self.disc_patch = (patch, patch, 1)
 
         # Number of filters in the first layer of G and D
         self.gf = 64
         self.df = 64
 
-        self.optimizer = Adam(0.0002, 0.5)
+        self.optimizer = Adam(0.0002,0.5)
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',
-                                   optimizer=self.optimizer,
-                                   metrics=['accuracy'])
+        self.discriminator.compile(loss='binary_crossentropy', optimizer=self.optimizer)  # 'binary_crossentropy'
 
         # -------------------------
         # Construct Computational
@@ -74,8 +72,8 @@ class Pix2Pix:
         valid = self.discriminator([fake_fluorescent, real_brightfield])
 
         self.combined = Model(inputs=[real_fluorescent, real_brightfield], outputs=[valid, fake_fluorescent])
-        self.combined.compile(loss=['binary_crossentropy', 'mae'],
-                              loss_weights=[1, 1],
+        self.combined.compile(loss=['binary_crossentropy', 'mae'],  # bce
+                              loss_weights=[1, 100],
                               optimizer=self.optimizer)
         if print_summary:
             self.print_summary()
@@ -85,55 +83,53 @@ class Pix2Pix:
 
         def conv2d(layer_input, filters, f_size=4, bn=True):
             """Layers used during downsampling"""
-            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
-            d = LeakyReLU(alpha=0.2)(d)
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same',activation='relu')(layer_input)
             if bn:
-                d = BatchNormalization(momentum=0.8)(d)
+                d = BatchNormalization(momentum=0.8)(d) #new epsilon addition
             return d
 
         def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
             """Layers used during upsampling"""
             u = UpSampling2D(size=2)(layer_input)
-            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
+            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same')(u)
             if dropout_rate:
                 u = Dropout(dropout_rate)(u)
             u = BatchNormalization(momentum=0.8)(u)
             u = Concatenate()([u, skip_input])
+            u = LeakyReLU(alpha=0.2)(u)
             return u
 
         # Image input
         d0 = Input(shape=self.img_shape)
 
         # Downsampling
-        d1 = conv2d(d0, self.gf, bn=False)  # 64
-        d2 = conv2d(d1, self.gf * 2)  # 128
-        d3 = conv2d(d2, self.gf * 4)  # 256
-        d4 = conv2d(d3, self.gf * 8)  # 512
-        d5 = conv2d(d4, self.gf * 16)  # 1024
-        # d6 = conv2d(d5, self.gf * 8)
-        # d7 = conv2d(d6, self.gf * 8)
+        d1 = conv2d(d0, self.gf, bn=False)
+        d2 = conv2d(d1, self.gf * 2)
+        d3 = conv2d(d2, self.gf * 4)
+        d4 = conv2d(d3, self.gf * 8)
+        d5 = conv2d(d4, self.gf * 16)
 
         # Upsampling
-        # u1 = deconv2d(d7, d6, self.gf * 8)
-        # u2 = deconv2d(d6, d5, self.gf * 8)
-        u3 = deconv2d(d5, d4, self.gf * 8)  # 512
-        u4 = deconv2d(u3, d3, self.gf * 4)  # 256
-        u5 = deconv2d(u4, d2, self.gf * 2)  # 128
-        u6 = deconv2d(u5, d1, self.gf)  # 64
+        u2 = deconv2d(d5, d4, self.gf * 8,dropout_rate=0.1)
+        u3 = deconv2d(u2, d3, self.gf * 4,dropout_rate=0.2)
+        u4 = deconv2d(u3, d2, self.gf * 2)
+        u5 = deconv2d(u4, d1, self.gf)
 
-        u7 = UpSampling2D(size=2)(u6)
-        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='sigmoid')(u7)
+        u6 = UpSampling2D(size=2)(u5)
+        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='sigmoid')(u6)
 
         return Model(d0, output_img)
 
     def build_discriminator(self):
 
-        def d_layer(layer_input, filters, f_size=4, bn=True):
+        def d_layer(layer_input, filters, f_size=4, bn=True, dropout_rate=0):
             """Discriminator layer"""
             d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
+            if dropout_rate > 0:
+                d = Dropout(dropout_rate)(d)
             if bn:
-                d = BatchNormalization(momentum=0.8)(d)
+                d = BatchNormalization(momentum=0.8,epsilon=0.01)(d)
             return d
 
         img_A = Input(shape=self.img_shape)
@@ -142,13 +138,14 @@ class Pix2Pix:
         # Concatenate image and conditioning image by channels to produce input
         combined_imgs = Concatenate(axis=-1)([img_A, img_B])
 
-        d1 = d_layer(combined_imgs, self.df, bn=False)
+        d1 = d_layer(combined_imgs, self.df) #
         d2 = d_layer(d1, self.df * 2)
         d3 = d_layer(d2, self.df * 4)
         d4 = d_layer(d3, self.df * 8)
-
-        validity = Conv2D(1, kernel_size=4, strides=1, padding='same')(d4)
-        # validity = Activation('sigmoid')(validity)
+        validity = Flatten()(d4)
+        validity = Dense(1)(validity)
+        validity = Activation('sigmoid')(validity)
+        # validity = Conv2D(1, kernel_size=4, strides=1, padding='same', activation='sigmoid')(d4)
 
         return Model([img_A, img_B], validity)
 
@@ -156,29 +153,41 @@ class Pix2Pix:
         start_time = datetime.datetime.now()
 
         # Adversarial loss ground truths
-        valid = np.ones((batch_size_in_patches,) + self.disc_patch)
-        fake = np.zeros((batch_size_in_patches,) + self.disc_patch)
+        # valid = np.ones((batch_size_in_patches,) + self.disc_patch)
+        # fake = np.zeros((batch_size_in_patches,) + self.disc_patch)
+        valid = np.ones((batch_size_in_patches,1))
+        fake = np.zeros((batch_size_in_patches,1))
+        d_loss = (0, 0)
+        # batch_generator =
 
         for epoch in range(epochs):
-            for batch_i, (real_brightfield_batch, real_fluorescent_batch) in enumerate(
-                    self.data_preper.load_images_as_batches(batch_size=batch_size_in_patches)):
+            for batch_i, (real_brightfield_batch, real_fluorescent_batch) in enumerate(self.data_preper.load_images_as_batches(batch_size=batch_size_in_patches)):
                 # ---------------------
                 #  Train Discriminator
                 # ---------------------
 
-                # Condition on B and generate a translated version
+                # fake_fluorescent_batch = self.generator.predict(real_brightfield_batch)
                 fake_fluorescent_batch = self.generator.predict(real_brightfield_batch)
 
                 # Train the discriminators (original images = real / generated = Fake)
-                d_loss_real = self.discriminator.train_on_batch([real_fluorescent_batch, real_brightfield_batch], valid)
-                d_loss_fake = self.discriminator.train_on_batch([fake_fluorescent_batch, real_brightfield_batch], fake)
-                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+                # leaning towards training generator better
+                if batch_i % 10 == 0: #supposed to give more time for the generator to train
+                    # disc_real_brightfield_batch, disc_real_fluorescent_batch = batch_generator.__next__()
+                    # batch_i += 1
+
+                    self.discriminator.trainable = True
+                    d_loss_real = self.discriminator.train_on_batch([real_fluorescent_batch, real_brightfield_batch],
+                                                                    valid)
+                    d_loss_fake = self.discriminator.train_on_batch([fake_fluorescent_batch, real_brightfield_batch],
+                                                                    fake)
+                    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
                 # -----------------
                 #  Train Generator
                 # -----------------
 
                 # Train the generators
+                self.discriminator.trainable = False
                 g_loss = self.combined.train_on_batch([real_fluorescent_batch, real_brightfield_batch],
                                                       [valid, real_fluorescent_batch])
 
@@ -186,8 +195,12 @@ class Pix2Pix:
                                        g_loss=g_loss, start_time=start_time)
 
                 # If at save interval => save generated image samples
-                # if ((batch_i + 1) % sample_interval_in_batches) == 0:
-            self.sample_images(epoch, 0)
+                if ((batch_i + 1) % sample_interval_in_batches) == 0:
+                    self.sample_images(epoch, batch_i + 1)
+
+                if d_loss < 0.001:
+                    raise InterruptedError('dloss was too low so generator has probably stopped learning at this point')
+
 
     def sample_images(self, epoch, batch_i):
 
@@ -195,12 +208,11 @@ class Pix2Pix:
         os.makedirs(images_root_dir, exist_ok=True)
         rows, cols = 3, 3  # num imgs over 3
         brightfield, fluorescent = self.data_preper.load_images_as_batches(batch_size=1, sample_size=3).__next__()
-        gen_fluorescent = self.generator.predict_on_batch(brightfield)
-        # gen_imgs = np.concatenate([brightfield, np.squeeze(gen_fluorescent), fluorescent])
+        gen_fluorescent = self.generator.predict(brightfield)
         gen_imgs = np.concatenate(
             [brightfield[:, :, :, 0], np.squeeze(gen_fluorescent[:, :, :, 0]), fluorescent[:, :, :, 0]])
 
-        # Rescale images 0 - 1
+        # TODO Rescale images 0 - 1 not sure if necessary
         gen_imgs = 0.5 * gen_imgs + 0.5
 
         titles = ['brightfield', 'gen fluorescent', 'real fluorescent']
@@ -225,9 +237,9 @@ class Pix2Pix:
         os.makedirs(progress_root_dir, exist_ok=True)
         os.makedirs(models_root_dir, exist_ok=True)
 
-        # save_model(model=self.combined, filepath=os.path.join(models_root_dir, 'combined_component'))
+        save_model(model=self.combined, filepath=os.path.join(models_root_dir, 'combined_component'))
         save_model(model=self.generator, filepath=os.path.join(models_root_dir, 'generator_model'))
-        # save_model(model=self.discriminator, filepath=os.path.join(models_root_dir, 'discriminator_model'))
+        save_model(model=self.discriminator, filepath=os.path.join(models_root_dir, 'discriminator_model'))
 
         fname = f"{datetime.datetime.now().strftime('%d-%m-%Y, %H:%M:%S')}.csv"
 
@@ -235,11 +247,12 @@ class Pix2Pix:
         progress_report.to_csv(os.path.join(progress_root_dir, fname), index=False)
 
     def load_model(self, target_path=None):
-        if not target_path:
+        if target_path is None:
             target_path = os.path.join(self.root_dir, 'models')
         # try:
-        # self.combined = load_model(filepath=os.path.join(target_path, 'combined_component'))
-        self.generator = load_model(filepath=os.path.join(target_path, 'generator_model'))
+        # self.combined = load_model(filepath=os.path.join(target_path, 'combined_component'),compile=False)
+        self.generator = load_model(filepath=os.path.join(target_path, 'generator_model'), compile=False)
+
         # self.discriminator = load_model(filepath=os.path.join(target_path, 'discriminator_model'))
         # except:
         # raise FileNotFoundError(f'Could not load models from path: {target_path}')
@@ -247,20 +260,21 @@ class Pix2Pix:
     def document_progress(self, curr_epoch, total_epochs, curr_batch, d_loss, g_loss, start_time):
         elapsed_time = datetime.datetime.now() - start_time
 
-        for k, v in zip(self.progress_report.keys(), [curr_epoch, curr_batch, g_loss[0], d_loss[0]]):
+        for k, v in zip(self.progress_report.keys(), [curr_epoch, curr_batch, g_loss[0], d_loss]):
             self.progress_report[k].append(v)
 
-        print("[Epoch %d/%d] [Batch %d] [D loss: %f, acc: %3d%%] [G loss: %f] time: %s" % (curr_epoch, total_epochs,
-                                                                                           curr_batch,
-                                                                                           d_loss[0],
-                                                                                           100 * d_loss[1],
-                                                                                           g_loss[0],
-                                                                                           elapsed_time))
+        print("[Epoch %d/%d] [Batch %d] [D loss: %f] [G loss: %f] time: %s" % (curr_epoch, total_epochs,
+                                                                               curr_batch,
+                                                                               d_loss,
+                                                                               g_loss[0],
+                                                                               elapsed_time))
 
     def load_model_predict_and_save(self):  # first image only
         self.load_model()
-        data_input = utils.load_numpy_array(self.data_preper.saved_input_imgs_fname)
-        data_output = utils.load_numpy_array(self.data_preper.saved_output_imgs_fname)
+        data_input = utils.load_numpy_array(self.data_preper.input_img_array_path)
+        data_output = utils.load_numpy_array(self.data_preper.output_img_array_path)
+        data_input = data_input[:2]
+        data_output = data_output[:2]
         data_input = utils.transform_dimensions(data_input, [0, 2, 3, 1])
         data_output = utils.transform_dimensions(data_output, [0, 2, 3, 1])
 
@@ -278,13 +292,14 @@ class Pix2Pix:
 
     def print_summary(self):
         self.generator.summary()
-        self.combined.summary()
-        models_dir = os.path.join(self.root_dir, 'pix2pix', 'models')
-        os.makedirs(models_dir, exist_ok=True)
-        plot_model(self.generator, to_file=os.path.join(models_dir, 'generator_model_plot.png'), show_shapes=True,
-                   show_layer_names=True)
-        plot_model(self.discriminator, to_file=os.path.join(models_dir, 'discriminator_model_plot.png'),
-                   show_shapes=True, show_layer_names=True)
+        self.discriminator.summary()
+        # self.combined.summary()
+        # models_dir = os.path.join(self.root_dir, 'pix2pix', 'models')
+        # os.makedirs(models_dir, exist_ok=True)
+        # plot_model(self.generator, to_file=os.path.join(models_dir, 'generator_model_plot.png'), show_shapes=True,
+        #            show_layer_names=True)
+        # plot_model(self.discriminator, to_file=os.path.join(models_dir, 'discriminator_model_plot.png'),
+        #            show_shapes=True, show_layer_names=True)
 
 
 if __name__ == '__main__':
@@ -293,8 +308,13 @@ if __name__ == '__main__':
     #           sys.executable + " pix2pix.py --size 192 >result.txt" +
     #           "' &")
 
-    batch_size = 75  # in patches
-    gan = Pix2Pix()
-    gan.train(epochs=1, batch_size_in_patches=batch_size, sample_interval_in_batches=-1)
-    gan.save_model_and_progress_report()
+    batch_size = 32
+    print_summary = True
+    sample_interval_in_batches = 53
+
+    gan = Pix2Pix(print_summary=print_summary)
+    try:
+        gan.train(epochs=100, batch_size_in_patches=batch_size, sample_interval_in_batches=sample_interval_in_batches)
+    finally:
+        gan.save_model_and_progress_report()
     # gan.load_model_predict_and_save()
--- Pix2Pix/pix_data_prepere.py
+++ Pix2Pix/pix_data_prepere.py
@@ -1,7 +1,7 @@
+import gc
 import os
-import sys
 
-import data_prepere
+import data_prepare
 import utils
 import numpy as np
 import getpass
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 USER = getpass.getuser().split("@")[0]
 DIRECTORY = "/home/%s/prediction3D" % USER
 os.makedirs(DIRECTORY, exist_ok=True)
+
 
 
 class pix2pix_data_prepare():
@@ -23,28 +24,43 @@ class pix2pix_data_prepare():
 
         self.img_size = (6, 128, 128)  # (z,y,x)
         self.img_size_rev = (self.img_size[1], self.img_size[2], self.img_size[0])
-        self.saved_input_imgs_fname = 'input_images_after_data_prepare.npy'
-        self.saved_output_imgs_fname = 'output_images_after_data_prepare.npy'
+        self.img_limit = 150
 
-        self.limit = 150
+        # self.org_type = "Nuclear-envelope/"
         self.org_type = "Mitochondria/"
-        self.images_paths = data_prepere.load_paths(self.org_type, limit=self.limit)
+        self.input_img_array_path = os.path.join(self.org_type, 'input_images_after_data_prepare_norm')
+        self.output_img_array_path = os.path.join(self.org_type, 'output_images_after_data_prepare_norm')
 
-        # self.sample_range = np.arange(len(self.train_x))
+        self.load_prep_images()
 
-        self.load_images()
-
-
-    def load_all_images_of_specific_organelle(self, data_input=None, data_output=None):
-        if data_input is None or data_output is None:
-            data_input = utils.load_numpy_array(self.saved_input_imgs_fname)
-            data_output = utils.load_numpy_array(self.saved_output_imgs_fname)
-
+    def get_patches(self, data_input=None, data_output=None):
         data_input = utils.transform_dimensions(data_input, [0, 2, 3, 1])
         data_output = utils.transform_dimensions(data_output, [0, 2, 3, 1])
-
         self.patches_input = utils.utils_patchify(data_input, self.img_size_rev, resize=True, over_lap_steps=1)
         self.patches_output = utils.utils_patchify(data_output, self.img_size_rev, resize=True, over_lap_steps=1)
+
+    def load_prep_images(self):
+        data_input,data_output = self.load_images_from_memory()
+        self.get_patches(data_input, data_output)
+        self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.patches_input, self.patches_output,
+                                                                                test_size=0.3,
+                                                                                random_state=3,
+                                                                                shuffle=True)
+        del self.patches_input, self.patches_output,data_input,data_output #,self.test_x, self.test_y
+        gc.collect()
+
+    def load_images_from_memory(self):
+        if len(os.listdir(DIRECTORY)) == 0:
+            images_paths = data_prepare.load_paths(self.org_type, limit=self.img_limit)
+            data_input, data_output = data_prepare.separate_data(images_paths, self.img_size)
+            utils.save_numpy_array(data_input, self.input_img_array_path)
+            utils.save_numpy_array(data_output, self.output_img_array_path)
+        else:
+            data_input = utils.load_numpy_array(self.input_img_array_path + '.npy')
+            data_output = utils.load_numpy_array(self.output_img_array_path + '.npy')
+
+        return data_input,data_output
+
 
     def load_images_as_batches(self, batch_size=16, sample_size=-1):
         if sample_size > 0:  # meant for saving an output
@@ -59,24 +75,3 @@ class pix2pix_data_prepare():
             brightfield_patches_samples = self.train_x[sampled_indexes]
             fluorescent_patches_samples = self.train_y[sampled_indexes]
             yield brightfield_patches_samples, fluorescent_patches_samples
-
-    def load_images(self):
-        data_input = None
-        data_output = None
-        if len(os.listdir(DIRECTORY)) == 0:
-            data_input, data_output = self.save_images_of_specific_organelle()
-        self.load_all_images_of_specific_organelle(data_input, data_output)
-        self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.patches_input, self.patches_output,
-                                                                                test_size=0.3,
-                                                                                random_state=3,
-                                                                                shuffle=True)
-        self.patches_input = None
-        self.patches_output = None
-        self.test_x = None
-        self.test_y = None
-
-    def save_images_of_specific_organelle(self):
-        data_input, data_output = data_prepere.separate_data(self.images_paths,self.img_size)
-        utils.save_numpy_array(data_input, "input_images_after_data_prepare")
-        utils.save_numpy_array(data_output, "output_images_after_data_prepare")
-        return data_input, data_output
