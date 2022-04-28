799a6920e2bf4fed6191629c4a397d0ed81f3eb5
ozlia <ozlia@post.bgu.ac.il>
2022-04-28 20:45:41 +0300

    add dropout and batch norm for overwriting

--- UNET/Unet.py
+++ UNET/Unet.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, Dropout, concatenate, MaxPooling2D, Input, \
-    MaxPool2D
+    MaxPool2D, BatchNormalization, ReLU
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Model
 from ICNN import ICNN
@@ -12,29 +12,30 @@ class Unet(ICNN):
     def __init__(self, input_dim=(128, 128, 6), batch_size=32, epochs=1000):
         inputs = Input(shape=input_dim)
 
-        self._filter_size = (3, 3)
+        self._filter_size = 3
         self._stride = (2, 2)
+        self._num_start_filters = 32
 
         # encoder: contracting path - downsample
         # 1 - downsample
-        f1, p1 = self.downsample_block(inputs, 16)
+        f1, p1 = self.downsample_block(inputs, 32)
         # 2 - downsample
-        f2, p2 = self.downsample_block(p1, 32)
+        f2, p2 = self.downsample_block(p1, 64)
         # 3 - downsample
-        f3, p3 = self.downsample_block(p2, 64)
+        f3, p3 = self.downsample_block(p2, 128)
         # 4 - downsample
-        f4, p4 = self.downsample_block(p3, 128)
+        f4, p4 = self.downsample_block(p3, 256)
         # 5 - bottleneck
-        bottleneck = self.double_conv_block(p4, 256)
+        bottleneck = self.double_conv_block(p4, 512)
         # decoder: expanding path - upsample
         # 6 - upsample
-        u6 = self.upsample_block(bottleneck, f4, 128)
+        u6 = self.upsample_block(bottleneck, f4, 256)
         # 7 - upsample
-        u7 = self.upsample_block(u6, f3, 64)
+        u7 = self.upsample_block(u6, f3, 128)
         # 8 - upsample
-        u8 = self.upsample_block(u7, f2, 32)
+        u8 = self.upsample_block(u7, f2, 64)
         # 9 - upsample
-        u9 = self.upsample_block(u8, f1, 16)
+        u9 = self.upsample_block(u8, f1, 32)
         # outputs
         outputs = Conv2D(input_dim[2], (1, 1), activation='sigmoid', name='decoder_output')(u9)
         # unet model with Keras Functional API
@@ -56,15 +57,19 @@ class Unet(ICNN):
 
     def double_conv_block(self, x, n_filters):
         # Conv2D then ReLU activation
-        x = Conv2D(n_filters, self._filter_size, padding="same", activation="relu")(x)
+        x = Conv2D(n_filters, self._filter_size, padding="same")(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
         # Conv2D then ReLU activation
-        x = Conv2D(n_filters, self._filter_size, padding="same", activation='relu')(x)
+        x = Conv2D(n_filters, self._filter_size, padding="same")(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
         return x
 
     def downsample_block(self, x, n_filters):
         f = self.double_conv_block(x, n_filters)
         p = MaxPool2D(self._stride)(f)
-        # p = Dropout(0.3)(p)
+        p = Dropout(0.3)(p)
         return f, p
 
     def upsample_block(self, x, conv_features, n_filters):
