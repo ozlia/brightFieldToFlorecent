afa589feb8e85811b581234f355b17c87887f1fb
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2022-06-09 15:03:53 +0300

    added channels pick by brightest for data_prepare.separate_data

---
+++ Pix2Pix/BeforeDataGenLiadUnet/data_handler.py
@@ -0,0 +1,79 @@
+import gc
+import os
+import data_prepare
+import utils
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+class data_handler():
+    def __init__(self):
+        self.brightfield_patches = None
+        self.fluorescent_patches = None
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+
+        self.img_size = (6, 64, 64)  # (z,y,x)
+        self.img_size_rev = (self.img_size[1], self.img_size[2], self.img_size[0])
+        self.img_limit = 150
+
+        # prep paths for input images
+
+        organelles = ["Mitochondria", "Nuclear-envelope", "Microtubules", "Endoplasmic-reticulum", "Action-filaments"]
+        self.org_type = organelles[0]
+
+        self.input_img_array_path = os.path.join(self.org_type, 'input_images_after_data_prepare_norm')
+        self.output_img_array_path = os.path.join(self.org_type, 'output_images_after_data_prepare_norm')
+
+        self.load_prep_images()
+
+    def load_prep_images(self):
+        brightfield_imgs, fluorescent_imgs = self.load_images_from_memory()
+        brightfield_imgs = utils.transform_dimensions(brightfield_imgs, [0, 2, 3, 1])
+        fluorescent_imgs = utils.transform_dimensions(fluorescent_imgs, [0, 2, 3, 1])
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(brightfield_imgs, fluorescent_imgs,
+                                                                                test_size=0.3,
+                                                                                random_state=3,
+                                                                                shuffle=True)
+
+        self.X_train = utils.utils_patchify(self.X_train, self.img_size_rev, resize=True, over_lap_steps=1)
+        self.y_train = utils.utils_patchify(self.y_train, self.img_size_rev, resize=True, over_lap_steps=1)
+
+        del brightfield_imgs, fluorescent_imgs
+        gc.collect()
+
+    def load_images_from_memory(self):
+        org_path = os.path.join(utils.DIRECTORY, f'{self.input_img_array_path}.npy')
+        if os.path.exists(org_path):
+            brightfield_imgs = utils.load_numpy_array(self.input_img_array_path + '.npy')
+            fluorescent_imgs = utils.load_numpy_array(self.output_img_array_path + '.npy')
+        else:
+            os.mkdir(os.path.join(utils.DIRECTORY, self.org_type))
+            images_paths = data_prepare.load_paths(self.org_type + '/', limit=self.img_limit)
+            brightfield_imgs, fluorescent_imgs = data_prepare.separate_data(images_paths, self.img_size,slice_by_brightest=True)
+            utils.save_numpy_array(brightfield_imgs, self.input_img_array_path)
+            utils.save_numpy_array(fluorescent_imgs, self.output_img_array_path)
+
+        return brightfield_imgs, fluorescent_imgs
+
+    def load_images_as_batches(self, batch_size=16, sample_size=-1, shuffle=False):
+        if shuffle:
+            curr_idx_array = np.arange(start=0, stop=len(self.X_train), step=1)
+
+        if sample_size > 0:  # meant for saving an output
+            batch_size = sample_size
+            n_batches = 1
+        else:
+            n_batches = int(len(self.X_train) / batch_size)
+
+        for i in range(n_batches):
+            if shuffle:  # can also do "lazy shuffle" that just picks from train_X without taking care of coverage
+                sampled_indexes = np.random.choice(curr_idx_array, batch_size, replace=False)
+                np.delete(curr_idx_array, sampled_indexes)
+            else:
+                sampled_indexes = np.arange(start=i * batch_size, stop=((i + 1) * batch_size))
+            brightfield_patches_samples = self.X_train[sampled_indexes]
+            fluorescent_patches_samples = self.y_train[sampled_indexes]
+            yield brightfield_patches_samples, fluorescent_patches_samples
--- data_prepare.py
+++ data_prepare.py
@@ -8,7 +8,6 @@ import utils
 import pandas as pd
 import csv
 
-
 TIFF_DF = pd.DataFrame()
 
 
@@ -32,6 +31,7 @@ def load_paths(org_type, limit=1):
 
     # files, x_pixels, y_pixels, color
 
+
 def load_paths_v2(org_type, limit=None):  # Doesn't compel to pass organelle with '/'. limit default to max over min
     fovs = []
     storage_folder = '/' + os.path.join('storage', 'users', 'assafzar', 'fovs', org_type)
@@ -43,7 +43,8 @@ def load_paths_v2(org_type, limit=None):  # Doesn't compel to pass organelle wit
             fovs.append(os.path.join(storage_folder, file))
     return fovs[:limit]
 
-def separate_data(fovs, img_size, multiply_img_z=1):
+
+def separate_data_with_omer_mess(fovs, img_size, multiply_img_z=1):
     unregistered_tiff = []
     bright_field = []
     fluorescent = []
@@ -89,11 +90,86 @@ def separate_data(fovs, img_size, multiply_img_z=1):
     fluorescent_array = np.array(fluorescent)
     return bright_field_array, fluorescent_array
 
+def separate_data(fovs, img_size, multiply_img_z=1, slice_by_brightest=False):
+    unregistered_tiff = []
+    bright_field = []
+    fluorescent = []
+    z, y, x = img_size
+    tiff_to_read = len(fovs)
+    print("Now Reading %d tiffs from disk, should take about %s (hr-min-sec)" % (
+        tiff_to_read, timedelta(seconds=20 * tiff_to_read)))
+    for i, tiff in enumerate(fovs):
+        print("reading tiff number %d out of %d" % ((i + 1), tiff_to_read))
+        start = datetime.now()
+        reader = AICSImage(tiff)
+        img = reader.data
 
-def image3d_prep(img_3d, type):
+        img = np.squeeze(img, axis=0)
+        n_channels = img.shape[0]
+        if n_channels <= 6:
+            continue
+        if slice_by_brightest:
+            prep_img = lambda single_brightfield_or_floro: slice_img(image3d_prep(single_brightfield_or_floro),num_z_slices=6)
+            bright_field.append(prep_img(img[n_channels - 1, :, :, :]))
+            fluorescent.append(prep_img(img[n_channels - 4, :, :, :]))
+        else:
+            mid_slice = np.int(0.5 * img.shape[1])
+            under_slice = int(mid_slice - z * multiply_img_z / 2)
+            for i in range(multiply_img_z):
+                img_3d = img[n_channels - 1, under_slice:under_slice + z, :, :]  # [bright_field, slice_z, all 2D image]
+
+                bright_field.append(image3d_prep(img_3d, ImgType.BRIGHT_FIELD))
+
+                img_3d = img[n_channels - 4, under_slice:under_slice + z, :, :]
+                fluorescent.append(image3d_prep(img_3d, ImgType.FLUORESCENT))
+                under_slice += z
+        stop = datetime.now()
+        print('Done, Time for this tiff: ', stop - start)
+    bright_field_array = np.array(bright_field)
+    fluorescent_array = np.array(fluorescent)
+    return bright_field_array, fluorescent_array
+
+
+def image3d_prep(img_3d):
     z, y, x = img_3d.shape
     img_3d_padded = np.zeros((z, 640, 896))
     for i in range(z):
         img_3d_padded[i] = cv2.resize(img_3d[i], (896, 640), interpolation=cv2.INTER_AREA)
     img_3d_norm = utils.norm_img(img_3d_padded)
     return img_3d_norm
+
+
+def get_brightest_layer(img_3d_channels_first) -> int:
+    '''
+    @param img_3d:
+    @return: int of z layer with brightest pixels (by mean only)
+    '''
+    brightest_z_layer = -1
+    max_mean = -1
+    z_layers = img_3d_channels_first.shape[0]
+    for curr_z_layer in range(z_layers):
+        curr_slice_pixel_wise_mean = np.mean(img_3d_channels_first[curr_z_layer])
+        if curr_slice_pixel_wise_mean > max_mean:
+            max_mean = curr_slice_pixel_wise_mean
+            brightest_z_layer = curr_z_layer
+    return brightest_z_layer
+
+
+def slice_img(img_3d_channels_first, num_z_slices=6):
+    '''
+
+    @param img_3d_channels_first:
+    @param chosen_z_layer:
+    @return: chooses upto 32 slices out of each direction (up and down) from the chosen slice. if not available, chooses randomly.
+    '''
+    assert len(img_3d_channels_first.shape) == 3, 'expecting 3d img with channels first'
+    assert num_z_slices % 2 == 0, 'expecting an attempt to take equal number of slices from above and below the chosen z slice, so total number of z slices must be even'
+    chosen_z_layer = get_brightest_layer(img_3d_channels_first)
+    z_slices = np.arange(start=0, stop=img_3d_channels_first.shape[0], step=1)
+    chosen_slices = z_slices[chosen_z_layer - (num_z_slices / 2):chosen_z_layer + (num_z_slices / 2)]
+    slices_left = num_z_slices - len(chosen_slices)
+    if slices_left > 0:
+        z_slices = np.delete(z_slices, chosen_slices)
+        complementing_slices = np.random.choice(z_slices, size=slices_left, replace=False)
+        chosen_slices = np.concatenate((chosen_slices, complementing_slices), axis=0)
+    return img_3d_channels_first[chosen_slices]
